fb86d9cbea94f80481c947e0aa0d2a0c946b35ac
dorian <dorianlegan@protonmail.com>
2022-04-08 15:16:17 +0100

    further api changes

---
+++ controllers/api_armor_controller.js
@@ -0,0 +1,139 @@
+const async = require("async");
+const Armor = require("../models/armor");
+const { body, validationResult } = require("express-validator");
+const Category = require("../models/category");
+
+exports.armor = async (req, res, next) => {
+  try {
+    const armor_data = await Armor.find({});
+    res.send(armor_data);
+  } catch (e) {
+    next(e);
+  }
+};
+
+exports.armor_detail = async (req, res, next) => {
+  try {
+    const armor = await Armor.findById(req.params.id);
+    if (armor) {
+      res.send(armor);
+    } else {
+      return res.status(404).json({ error: "Weapon not found" });
+    }
+  } catch (e) {
+    return res.status(404).json({ error: e });
+  }
+};
+
+// Handle weapon item create on POST.
+exports.armor_create_post = [
+  body("name", "name must be specified").trim().isLength({ min: 1 }).escape(),
+  body("val", "val must be specified and in the range 1 - 9999")
+    .trim()
+    .isLength({ min: 1 })
+    .isInt({ min: 1, max: 9999 })
+    .escape(),
+  body("weight", "weight must be specified and in the range 1 - 999")
+    .trim()
+    .isLength({ min: 1 })
+    .isInt({ min: 1, max: 999 })
+    .escape(),
+  body("condition", "condition must be specified and in the range 1 - 99")
+    .trim()
+    .isLength({ min: 1 })
+    .isInt({ min: 1, max: 99 })
+    .escape(),
+
+  body("damage", "damage must be specified and in the range 1 - 999")
+    .trim()
+    .isLength({ min: 1 })
+    .isInt({ min: 1, max: 99 })
+    .escape(),
+
+  async (req, res, next) => {
+    const errors = validationResult(req);
+    const cat = await Category.findOne({ name: "Armor" });
+    const armor = new Armor({
+      name: req.body.name,
+      val: req.body.val,
+      weight: req.body.weight,
+      condition: req.body.condition, //genre is an array of genre ids
+      effects: req.body.effects,
+      category: cat,
+    });
+    if (!errors.isEmpty()) {
+      res.send(errors);
+    } else {
+      try {
+        await armor.save();
+        res.send({ armor: armor, message: "Armor created" });
+      } catch (e) {
+        res.send(e);
+      }
+    }
+  },
+];
+
+/*
+// Handle weapon item update on POST.
+exports.weapon_update_put = [
+  body("name", "name must be specified").trim().isLength({ min: 1 }).escape(),
+  body("val", "val must be specified and in the range 1 - 9999")
+    .trim()
+    .isLength({ min: 1 })
+    .isInt({ min: 1, max: 9999 })
+    .escape(),
+  body("weight", "weight must be specified and in the range 1 - 999")
+    .trim()
+    .isLength({ min: 1 })
+    .isInt({ min: 1, max: 999 })
+    .escape(),
+  body("condition", "condition must be specified and in the range 1 - 99")
+    .trim()
+    .isLength({ min: 1 })
+    .isInt({ min: 1, max: 99 })
+    .escape(),
+
+  body("damage", "damage must be specified and in the range 1 - 999")
+    .trim()
+    .isLength({ min: 1 })
+    .isInt({ min: 1, max: 99 })
+    .escape(),
+
+  async (req, res, next) => {
+    const errors = validationResult(req);
+    const cat = await Category.findOne({ name: "Weapons" });
+    const weapon = new Weapon({
+      name: req.body.name,
+      val: req.body.val,
+      weight: req.body.weight,
+      dam: req.body.damage,
+      condition: req.body.condition, //genre is an array of genre ids
+      category: cat,
+      _id: req.params.id,
+    });
+
+    if (!errors.isEmpty()) {
+      res.send(errors);
+    } else {
+      try {
+        await Weapon.findByIdAndUpdate(req.params.id, weapon);
+        return res.status(200).json({ id: req.params.id });
+      } catch (e) {
+        next(e);
+      }
+    }
+  },
+];
+
+// Handle weapon item delete on POST.
+exports.weapon_delete_post = async (req, res, next) => {
+  try {
+    await Weapon.findByIdAndRemove(req.params.id);
+    return res.status(200).json();
+  } catch (e) {
+    console.log("error");
+    return res.status(400).json({ error: "User could not be removed" });
+  }
+};
+*/
--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,7 @@ const express = require("express");
 
 const router = express.Router();
 const weaponController = require("../controllers/api_weapon_controller");
+const armorController = require("../controllers/api_armor_controller");
 
 // weapon CRUD
 
@@ -14,5 +15,19 @@ router.post("/weapons/", weaponController.weapon_create_post);
 router.put("/weapons/:id", weaponController.weapon_update_put);
 
 router.delete("/weapons/:id", weaponController.weapon_delete_post);
+
+// armor CRUD
+
+router.get("/armor", armorController.armor);
+
+router.get("/armor/:id", armorController.armor_detail);
+/*
+
+router.post("/armor/", armorController.armor_create_post);
+
+router.put("/armor/:id", armorController.armor_update_put);
+
+router.delete("/armor/:id", armorController.armor_delete_post);
+*/
 
 module.exports = router;
